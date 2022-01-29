0187b85f460c726e91b8ae1dde39882c45991a6f
Muh Sukma Saputra <muh.saputra@kanmogroup.com>
2022-01-30 01:34:04 +0700

    tes

--- Models/ReportUser/ReportUser.cs
+++ Models/ReportUser/ReportUser.cs
@@ -9,7 +9,7 @@ namespace Warehouse_API.Models.ReportUser
     {
         public string UserName { get; set; }
         public string RoleName { get; set; }
-        public DateTime LastLoginDate { get; set; }
+        public string LastLoginDate { get; set; }
         public string Active { get; set; }
         public string Received { get; set; }
         public string Putaway { get; set; }
