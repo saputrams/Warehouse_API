7e35e3e4daebe7966e8746df83ee8196de9f9afc
Muh Sukma Saputra <muh.saputra@kanmogroup.com>
2022-01-30 01:58:12 +0700

    salah

--- Controllers/OrderCancelledController.cs
+++ Controllers/OrderCancelledController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using Warehouse_API.Helper;
 using Warehouse_API.Models;
 using Warehouse_API.Models.Item;
+using Warehouse_API.Models.Order;
 
 namespace Warehouse_API.Controllers
 {
@@ -24,19 +25,34 @@ namespace Warehouse_API.Controllers
         public IHttpActionResult Post(string orderId, string token, string userName)
         {
             ResponseData response = new ResponseData();
+            List<Order> order = new List<Order>();
+            IEnumerable<OrderDetail> orderDetail = null;
+            Order orderData;
             try
             {
-                IEnumerable<Item> itemDamage = connection.Get<Item>("OrderCancelled_POST",
-                    new { orderId,token, userName }).ToList();
+                IEnumerable<OrderFetch> data = connection.Get<OrderFetch>("OrderCancelled_POST", new { orderId,token, userName }).ToList();
+                foreach (var s in data.Select(x => new { x.OrderId, x.OrderNo, x.OrderDate, x.Status, x.OrderType }).Distinct().ToList())
+                {
+                    orderData = new Order();
+                    orderDetail = data.Where(e => e.OrderId == s.OrderId).Select(y => new OrderDetail { OrderDetailId = y.OrderDetailId, ItemId = y.ItemId, ItemNo = y.ItemNo, ItemDesc = y.ItemDesc, Qty = y.Qty, QtyScanning = y.QtyScanning }).Distinct().ToList();
+
+                    orderData.OrderId = s.OrderId;
+                    orderData.OrderNo = s.OrderNo;
+                    orderData.OrderDate = s.OrderDate;
+                    orderData.OrderType = s.OrderType;
+                    orderData.Status = s.Status;
+                    orderData.OrderDetail = (List<OrderDetail>)orderDetail;
+
+                    order.Add(orderData);
+                }
 
 
                 response.Message = "SUCCESS";
-                response.Data = itemDamage;
+                response.Data = order;
                 return this.Content(HttpStatusCode.OK, response);
             }
             catch (Exception error)
             {
-
                 response.Message = error.Message;
                 response.Data = null;
                 return this.Content(HttpStatusCode.BadRequest, response);
